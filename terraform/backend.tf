64f6245d644c3dd778d595a78917b1ae26f88fa0
aliyanalisamnani <aliyananwer786@gmail.com>
2025-06-20 00:43:13 +0500

    first deployment

--- terraform/backend.tf
+++ terraform/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket       = "anas-terraform-state-bucket"
+    bucket       = "aliyan-traform-bucket"
     key          = "todo-swarm/terraform.tfstate"
     region       = "us-east-1"    
     encrypt      = true
